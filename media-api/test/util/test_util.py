43a041be16e9756afbd84e1e99133c3133a826b9
Thach.Le <thachln@gmail.com>
2024-08-31 23:09:19 +0700

    Code function "find_file_csv_fieno_frame_idx" and make a unit test case.

--- media-api/test/util/test_util.py
+++ media-api/test/util/test_util.py
@@ -1,15 +1,24 @@
 import unittest
 from util import find_file_csv_fieno
+from util import find_file_csv_fieno_frame_idx
 
 class MyTestCase(unittest.TestCase):
     def test_find_file_csv_fieno(self):
-        folder_path = r'\Data_2024\Map Keyframe\map-keyframes-b1\map-keyframes'
-        file_path = r'\Data_2024\Keyframe\Keyframes_L01\keyframes\L01_V001\001.jpg'
+        folder_path = r'\AIC-2024\Data_2024\Map Keyframe\map-keyframes-b1\map-keyframes'
+        file_path = r'\AIC-2024\Data_2024\Keyframe\Keyframes_L01\keyframes\L01_V001\001.jpg'
 
         filecsv, n = find_file_csv_fieno(folder_path, file_path)
         self.assertEqual(filecsv, 'L01_V001.csv')
         self.assertEqual(n, 1)
 
+    def test_find_file_csv_fieno_frame_idx(self):
+        folder_path = r'\AIC-2024\Data_2024\Map Keyframe\map-keyframes-b1\map-keyframes'
+        file_path = r'\AIC-2024\Data_2024\Keyframe\Keyframes_L01\keyframes\L01_V001\001.jpg'
+
+        filecsv, n, frame_idx = find_file_csv_fieno_frame_idx(folder_path, file_path)
+        self.assertEqual(filecsv, 'L01_V001.csv')
+        self.assertEqual(n, 1)
+        self.assertEqual(frame_idx, 0)
 
 if __name__ == '__main__':
     unittest.main()
--- media-api/util/__init__.py
+++ media-api/util/__init__.py
@@ -1,4 +1,40 @@
 import os
+import pandas as pd
+
+def get_frame_idx_for_n(df, n_value):
+    # Filter the DataFrame where 'n' equals 'n_value'
+    result = df[df['n'] == n_value]
+
+    # Check if any row matches the condition
+    if not result.empty:
+        # Get the value of 'frame_idx' for the matched row
+        frame_idx = result.iloc[0]['frame_idx']
+        print('Type of var frame_idx:', frame_idx.dtype)
+
+        return int(frame_idx)
+    else:
+        return None  # Return None if no matching row is found
+
+def find_file_csv_fieno_frame_idx(folder_path: str, file_path: str):
+    '''
+    Process file_path to extract the last sub folder and filename.
+    Then lookup in the folder to get file .csv, sequence no of file, and frame index
+    :param folder_path: contains files of .csv such as L01_V001.csv, L01_V002.csv,...L12_V030.csv
+    :param file_path: is a string such as '…\L01_V001\001.jpg', …/L01_V001/001.jpg
+    :return: filename .csv in the folder_path, n, frame idex
+    Ex: L01_V001.csv, 1, 0
+    '''
+
+    filecsv, fileno = find_file_csv_fieno(folder_path, file_path)
+
+    filecsv_path = os.path.join(folder_path, filecsv)
+
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(filecsv_path)
+
+    frame_idx = get_frame_idx_for_n(df, fileno)
+
+    return filecsv, fileno, frame_idx
 
 def find_file_csv_fieno(folder_path: str, file_path: str):
     '''

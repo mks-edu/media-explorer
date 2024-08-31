9dfb2b5faf0fe5d2d40f6dc4f05c0a8a022703af
Thach.Le <thachln@gmail.com>
2024-08-31 22:18:48 +0700

    Code function find_file_csv_fieno.

--- media-api/util/__init__.py
+++ media-api/util/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 def find_file_csv_fieno(folder_path: str, file_path: str):
     '''
     Process file_path to extract the last sub folder and filename.
@@ -8,4 +10,18 @@ def find_file_csv_fieno(folder_path: str, file_path: str):
     Ex: L01_V001.csv, 1
     '''
 
-    return "", 0
+    last_subfolder, file_name_without_extension = get_last_subfolder_and_filename(file_path)
+
+    filecsv = last_subfolder + ".csv"
+    fileno = int(file_name_without_extension)
+
+    return filecsv, fileno
+
+def get_last_subfolder_and_filename(file_path):
+    # Get the last subfolder
+    last_subfolder = os.path.basename(os.path.dirname(file_path))
+
+    # Get the file name without extension
+    file_name_without_extension = os.path.splitext(os.path.basename(file_path))[0]
+
+    return last_subfolder, file_name_without_extension

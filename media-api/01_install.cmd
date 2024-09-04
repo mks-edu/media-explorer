7709fd3a0cd0e94647fc840923c46bc47d0aedfa
Thach.Le <thachln@gmail.com>
2024-09-04 07:27:53 +0700

    Update libraries to run CLIP with CUDA.

--- media-api/01_install.cmd
+++ media-api/01_install.cmd
@@ -7,9 +7,9 @@ python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
 
 
-REM pip install torch==2.2.2+cu121 torchaudio torchvision==0.17.2+cu121 -f https://download.pytorch.org/whl/torch_stable.html
-REM Try Torch 23.1
-pip3install torch==2.3.1+cu121 torchaudio==2.3.1+cu121 torchvision==0.18.1+cu121 -f https://download.pytorch.org/whl/torch_stable.html
+REM pip3 install torch==2.2.2+cu121 torchaudio torchvision==0.17.2+cu121 -f https://download.pytorch.org/whl/torch_stable.html
+REM Try Torch 2.3.1:Numpy is not available
+pip install torch==2.3.1+cu121 torchaudio==2.3.1+cu121 torchvision==0.18.1+cu121 -f https://download.pytorch.org/whl/torch_stable.html
 
 REM Install CLIP
 pip install git+https://github.com/openai/CLIP.git
